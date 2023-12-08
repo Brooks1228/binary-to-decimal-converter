cda2e410de851292b5a0413e86f272b3f4cad195
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2023-12-08 09:30:04 -0500

    finished program 12/8/23

--- Module1.vb
+++ Module1.vb
@@ -1,4 +1,6 @@
-﻿Module Module1
+﻿Imports System.Threading
+
+Module Module1
     'brooks keller
     '12/7/23
     'binary and decimal converter
@@ -12,9 +14,10 @@
             If input = "B" Then
                 toBinary()
             ElseIf input = "D" Then
-
+                toDecimal()
             Else
                 Console.WriteLine("quitting program . . .")
+                Thread.Sleep(500)
                 quit = True
             End If
         Loop While quit = False
@@ -35,12 +38,27 @@
             int = Math.Floor(int / 2)
         End While
         Console.WriteLine($"{input} in binary is {binstring}")
+        Console.WriteLine("press any key to continue...")
+        Console.ReadKey()
     End Sub
-    Function toDecimal()
+    Sub toDecimal()
         Dim input As String
+        Dim num As Integer
+        Dim totalnum As Integer
 
-        For i = 0 To input.Length - 1
+        Console.WriteLine()
+        Console.Write($"please enter some binary and i will attempt to make it an integer >> ")
+        input = Console.ReadLine
+        For i As Integer = 0 To input.Length - 1
+            If input(i) = "1" Then
+                num = 1 * 2 ^ (input.Length - 1 - i)
+                totalnum += num
+            ElseIf input(i) = "0" Then
+                num = 0
+                totalnum += num
+            End If
 
         Next
-    End Function
+        Console.WriteLine($"{input} to a decimal is {totalnum}")
+    End Sub
 End Module
